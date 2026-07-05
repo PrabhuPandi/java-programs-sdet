35fba1f7185ee0004d7297ef40f352e5ad95b7e5
PrabhuPandi <prabhupandi@gmail.com>
2026-07-06 00:16:20 +0530

    Add basic type conversion and number logic examples

---
+++ src/main/java/basic/typeconversion/convertIntToString.java
@@ -0,0 +1,9 @@
+package basic.typeconversion;
+
+public class convertIntToString {
+    public static void main(String[] args) {
+        int number=123456;
+        String str=String.valueOf(number);
+        System.out.println("Converted String is "+str);
+    }
+}s
